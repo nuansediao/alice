60c46eeafb55b5c31bfbb6b632e27e19875cc254
nuansediao <519962738@qq.com>
2016-05-17 16:16:52 +0800

    修改使用postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 var gulp = require('gulp'),
 	imagemin = require('gulp-imagemin'),
-	less = require('gulp-less'),
 	minifycss = require ('gulp-minify-css'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
 	clean = require('gulp-clean'),
 	spriter = require('gulp-css-spriter'),
-	browserSync = require('browser-sync').create();
-	fileinclude = require('gulp-file-include');
+	browserSync = require('browser-sync').create(),
+	fileinclude = require('gulp-file-include'),
+	postcss = require('gulp-postcss'),
+	gutil = require('gulp-util');
 
-
-
+	//postcss的小插件
+  alias = require('postcss-alias'),
+  crip = require('postcss-crip'),
+  magician = require('postcss-font-magician'),
+  triangle = require('postcss-triangle'),
+  circle = require('postcss-circle'),
+  linkColors = require('postcss-all-link-colors'),
+  center = require('postcss-center'),
+  clearfix = require('postcss-clearfix'),
+  position = require('postcss-position'),
+  size = require('postcss-size'),
+  // verthorz = require('postcss-verthorz'),
+  colorShort = require('postcss-color-short');
 
 //img处理
 gulp.task('img',function(){
@@ -22,29 +34,33 @@ gulp.task('img',function(){
 });
 
 
-//less处理
-gulp.task('less',function(){
-	var cssDrc = 'drc/less/**',
-		cssSrc = 'drc/css';
-	gulp.src(cssDrc)
-		.pipe(less())
-		.pipe(gulp.dest(cssSrc));
-
+//运行注解是彩色的
+gulp.task('default', function () {
+    gutil.log('message');
+    gutil.log(gutil.colors.red('error'));
+    gutil.log(gutil.colors.green('message:') + "some");
 });
 
-
-//css处理
-gulp.task('css',function(){
-	var cssDrc = 'drc/css/**',
-		cssSrc = 'src/css';
-	gulp.src(cssDrc)
-		// .pipe(less())
-		// .pipe(minifycss())
-		// .pipe(rename({suffix:'.min'}))
-		.pipe(gulp.dest(cssSrc));
-
+gulp.task('css', function () {
+	//安装postcss插件的地方
+  var processors = [
+			alias,
+      crip,
+      magician,
+      triangle,
+      circle,
+      linkColors,
+      center,
+      clearfix,
+      position,
+      size,
+      // verthorz,
+      colorShort
+  ];
+  return gulp.src('./drc/css/*.css')
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./src/css/'));
 });
-
 
 //js处理
 gulp.task('js',function(){
@@ -56,20 +72,6 @@ gulp.task('js',function(){
 		.pipe(gulp.dest(jsSrc));
 });
 
-//雪碧图处理
-gulp.task('spriter',function(){
-	var timestamp=+newDate();
-	//需要自动合并雪碧图的样式文件
-	returngulp.src('./drc/image/**')
-					//生成的spriter的位置
-	.pipe(spriter({'spriteSheet':'./drc/image/sprite'+timestamp+'.png',
-		//生成样式文件图片引用地址的路径
-		//如下将生产：backgound:url(../images/sprite20324232.png)
-		'pathToSpriteSheetFromCSS':'./drc/image/sprite'+timestamp+'.png'
-	}))
-	.pipe(gulp.dest('./src'));
-});
-
 
 // 清空图片、样式、js
 gulp.task('clean', function() {
@@ -79,28 +81,21 @@ gulp.task('clean', function() {
 
 //默认运行程序
 gulp.task('default',['clean'],function(){
-	gulp.start('html','less','css','img','js');
+	gulp.start('postcss','html','less','css','img','js');
 });
 
 // 静态服务器
 gulp.task('browser-sync', function() {
     browserSync.init({
-    	files: "./src/**",
+    	files: "./drc/**",
         server: {
             baseDir: "src/"  //index.html在哪里
         }
     });
 });
 
-// // html处理
-// gulp.task('html',function(){
-// 	var htmlDrc = 'src/*.html',
-// 		htmlSrc = 'src/';
-// 	gulp.src(htmlDrc)
-// 		.pipe(gulp.dest(htmlSrc));
-// });
 
-//配置 HTML 模块化处理  引导时这么写 “@@include('templates/footer.html')”             
+//配置 HTML 模块化处理  引导时这么写 “@@include('templates/footer.html')”
 gulp.task('html',function(){
 	gulp.src('./drc/*.html')
     .pipe(fileinclude({
@@ -108,19 +103,19 @@ gulp.task('html',function(){
       basepath: '@file'
     }))
     .pipe(gulp.dest('./src'));
-	
+
 });
 
 
 //监听任务，运行语句 gulp watch
 
 gulp.task('watch',['browser-sync'], function(){
-		gulp.start('less','css','html','img','js');
+		gulp.start('css','html','img','js');
 
 	//监听html
 	gulp.watch('./drc/*.html',function(){
 		gulp.run('html');
-		
+
 	});
 
 	//监听css
@@ -128,12 +123,6 @@ gulp.task('watch',['browser-sync'], function(){
 		gulp.run('css');
 	});
 
-
-	//监听less
-	gulp.watch('./drc/less/**',function(){
-		gulp.run('less');
-		gulp.run('css');
-	});
 
 	//监听js
 	gulp.watch('./drc/js/*.js',function(){
@@ -146,4 +135,3 @@ gulp.task('watch',['browser-sync'], function(){
 	});
 
 });
-
